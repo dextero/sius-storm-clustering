5bf9522cf68d5db407091eeb5e39706e51b982ad
dextero <hukutizuviki+git@gmail.com>
2015-06-02 11:32:45 +0200

    adjust min density

--- src/main/pl/edu/agh/sius/clustering/Constants.java
+++ src/main/pl/edu/agh/sius/clustering/Constants.java
@@ -10,6 +10,6 @@ public class Constants {
     public static final double MAX_VARIANCE = 0.05;
 
     public static final int TOTAL_CUBES_PER_SPACE = DIM_SIZE * DIM_CUBES_PER_BOLT;
-    public static final double DENSITY_MIN = 3000.0 / (double)(TOTAL_CUBES_PER_SPACE * TOTAL_CUBES_PER_SPACE);
+    public static final double DENSITY_MIN = 1.0 + 0.01;
     public static final double CUBE_SIZE = Constants.DIMENSION_SIZE / ((double) (Constants.TOTAL_CUBES_PER_SPACE));
 }
